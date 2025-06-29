0922fbbcec10e8da4c1b257bd1a2f8513ebc45cc
silvia730 <silvianjeri730@gmail.com>
2025-06-29 22:01:31 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -448,4 +448,4 @@ if __name__ == '__main__':
         db.create_all()
           port = int(os.environ.get('PORT', 5000))  # Get from .env or use 5000
     app.run(debug=True, host='0.0.0.0', port=port)
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    
