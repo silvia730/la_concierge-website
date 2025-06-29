6e953593ff0cff7ab05559b9f861fbc5b05dadc3
silvia730 <silvianjeri730@gmail.com>
2025-06-29 22:04:30 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -446,6 +446,6 @@ def health_check():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-          port = int(os.environ.get('PORT', 5000))  # Get from .env or use 5000
+    port = int(os.environ.get('PORT', 5000))  # Get from .env or use 5000
     app.run(debug=True, host='0.0.0.0', port=port)
     
